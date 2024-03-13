d976bce0cc45bbb1b7f5e66d973eee17ccc9cc15
Paul Woodland <pwoodland564@gmail.com>
2024-03-13 12:28:17 -0600

    Updated troubleshooting
    Still need help with adding even listener

--- static/main.js
+++ static/main.js
@@ -1,12 +1,20 @@
-console.log("hello?")
+console.log("hello?") // testing my console works
 
-let tableCells = document.getElementsByClassName('editable');
+let tableCells = document.getElementsByClassName('editable'); // getting all the <td>s
 
+proveEvent(); // testing my function works
 
+// just more tests so I know it works
+// it isn't being called/invoked when I 'focusout' of a td
 function proveEvent() {
     console.log("event happended");
+    alert("hello there");
 }
 
 for (let i = 0; i < tableCells.length; i++) {
     tableCells[i].addEventListener('focusout', proveEvent);
+    console.log("event listener added to: " + tableCells[i]);   // this line is not running
 }
+
+
+console.log(tableCells); // logs sucessfully, the data is there
