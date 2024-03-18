bf891efa2f9b1e5d05c86953e7770884c0a5fb63
Paul Woodland <pwoodland564@gmail.com>
2024-03-18 11:33:32 -0600

    edit existing complete
    Make async api call to backend and update database

--- static/main.js
+++ static/main.js
@@ -1,26 +1,28 @@
-console.log("hello?") // testing my console works
-
 const tableCells = document.getElementsByClassName('editable'); // getting all the <td>s
 
-proveEvent(); // testing my function works
-
-// just more tests so I know it works
-// it isn't being called/invoked when I 'focusout' of a td
-function proveEvent() {
-    console.log("event happended");
-}
-
+// adding the event listener to every td
 for (let i = 0; i < tableCells.length; i++) {
-    tableCells[i].addEventListener('focusout', proveEvent);
-    console.log("event listener added to: " + tableCells[i]);   // this line is not running
+    tableCells[i].addEventListener('focusout', sendData);
 }
 
-/*
-console.log(tableCells); // logs sucessfully, the data is there
-
-
-const testElement = document.getElementById('table');
-console.log(testElement);
-testElement.addEventListener('click', proveEvent);
-
-*/
+// getting the row for unfocused td
+async function sendData() {
+    console.log("Getting row for unfocused <td>");
+    console.log(this.parentNode.textContent);
+    try {
+        const response = await fetch('/welds/edit', {
+            method: "POST",
+            body: JSON.stringify(this.parentNode.textContent),
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        });
+        console.log("sent the data");
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
